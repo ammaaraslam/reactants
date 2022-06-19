8a680329c3863db63e7a4630cb89a951ea62a61c
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-06-19 16:22:36 +0530

    added next-auth authentication - working

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
+
   return (
     <div>
       <Head>
@@ -11,9 +16,39 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-red-700">
-        <h2>hello</h2>
-      </main>
+      <main className="bg-red-700"></main>
+    </div>
+  );
+}
+
+export function Header() {
+  const { data: session } = useSession();
+
+  const handleSignin = (e) => {
+    e.preventDefault();
+    signIn();
+  };
+
+  const handleSignout = (e) => {
+    e.preventDefault();
+    signOut();
+  };
+
+  return (
+    <div className="header">
+      <Link href="/">
+        <a className="logo">NextAuth.js</a>
+      </Link>
+      {session && (
+        <a href="#" onClick={handleSignout} className="btn-signin">
+          Sign out
+        </a>
+      )}
+      {!session && (
+        <a href="#" onClick={handleSignin} className="btn-signin">
+          Sign in
+        </a>
+      )}
     </div>
   );
 }
