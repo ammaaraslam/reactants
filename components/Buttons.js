6a8eec28e41e5bd752f9401737fe671b77f37f2e
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-06-25 11:56:17 +0530

    app colors, homepage hero complete

--- components/Buttons.js
+++ components/Buttons.js
@@ -3,7 +3,7 @@ export const PrimaryButton = ({ handleOnClick, children, type, textColor }) => {
     return (
       <button
         type="button"
-        className={`p-2 w-24 bg-primary rounded-xl inline-flex justify-center items-center hover:scale-105 transition-all duration-200 font-roboto font-bold text-${textColor} text-xl text-center`}
+        className={`p-2 w-24 bg-primary inline-flex justify-center items-center hover:translate-x-1 hover:-translate-y-1 hover:shadow-primaryShadow transition-all duration-200 font-roboto font-extrabold text-text dark:text-darkBackground text-xl text-center`}
         onClick={handleOnClick}
       >
         {children}
@@ -54,7 +54,7 @@ export const SecondaryButton = ({
     return (
       <button
         type="button"
-        className={`p-4 w-fit bg-secondary rounded-xl inline-flex justify-center items-center hover:scale-105 transition-all duration-200 font-roboto font-extrabold text-${textColor} text-3xl text-center`}
+        className={`p-4 w-fit bg-secondary text-background dark:text-darkBackground  inline-flex justify-center items-center hover:translate-x-1 hover:-translate-y-1 hover:shadow-secondaryShadow hover:text-secondaryDark transition-all duration-200 font-roboto font-extrabold  text-3xl text-center`}
         onClick={handleOnClick}
       >
         {children}
@@ -65,6 +65,46 @@ export const SecondaryButton = ({
       <button
         type="button"
         className={`p-4 w-fit bg-secondary rounded-xl inline-flex justify-center items-center hover:scale-105 transition-all duration-200 font-roboto font-extrabold text-${textColor} text-2xl text-center`}
+        onClick={handleOnClick}
+      >
+        {children}
+      </button>
+    );
+  }
+};
+
+export const PrimaryOutlineButton = ({
+  handleOnClick,
+  children,
+  type,
+  textColor,
+}) => {
+  if (type == "small") {
+    return (
+      <button
+        type="button"
+        className={`p-2 w-24 bg-transparent border-2 border-primary inline-flex justify-center items-center hover:bg-primary transition-all duration-200 font-roboto font-bold text-primary hover:text-background hover:dark:text-darkBackground text-md text-center`}
+        onClick={handleOnClick}
+      >
+        {children}
+      </button>
+    );
+  }
+  if (type == "big") {
+    return (
+      <button
+        type="button"
+        className="p-5 w-fit bg-transparent rounded-xl inline-flex justify-center items-center hover:scale-105 transition-all duration-200 font-extrabold text-darkText text-2xl text-center"
+        onClick={handleOnClick}
+      >
+        {children}
+      </button>
+    );
+  } else {
+    return (
+      <button
+        type="button"
+        className="p-3 w-fit bg-transparent border-1 border-primary rounded-xl inline-flex justify-center items-center hover:scale-105 transition-all duration-200 font-bold text-darkText text-xl text-center"
         onClick={handleOnClick}
       >
         {children}
--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import { PrimaryButton } from "./Buttons";
+import { PrimaryOutlineButton } from "./Buttons";
 import Link from "next/link";
+import React, { useState, useEffect } from "react";
 
 const Header = () => {
   const { data: session } = useSession();
@@ -14,39 +15,82 @@ const Header = () => {
     e.preventDefault();
     signOut();
   };
+  const [navActive, setNavActive] = useState(false);
+  const listenScrollEvent = () => {
+    window.scrollY > 13 ? setNavActive(true) : setNavActive(false);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+    return () => {
+      window.removeEventListener("scroll", listenScrollEvent);
+    };
+  }, []);
 
   return (
-    <header className="fixed p-3 w-full h-fit flex items-center justify-between bg-background dark:bg-darkBackground bg-opacity-80 dark:bg-opacity-80 backdrop-blur-lg backdrop-saturate-150 text-darkText dark:text-text ">
-      <div className="p-0 ml-2">
-        <span className="text-2xl font-bold">Reactants</span>
-      </div>
-      <div className="inline-flex font-bold text-xl font-roboto tracking-wider">
-        <li className="list-none mr-8 decoration-2 underline-offset-4 hover:underline hover:text-tertiary decoration-tertiary transition-all duration-200">
-          <Link href="/">Contribute</Link>
-        </li>
-        <li className="list-none decoration-2 underline-offset-4 hover:underline hover:text-tertiary decoration-tertiary transition-all duration-200">
-          <Link href="/">About</Link>
-        </li>
-      </div>
-      {session && (
-        <PrimaryButton
-          handleOnClick={handleSignout}
-          type="small"
-          buttonText="Sign Out"
-        >
-          Sign Out
-        </PrimaryButton>
+    <>
+      {navActive && (
+        <header className="fixed p-3 w-full h-fit flex items-center justify-between bg-background dark:bg-darkBackground bg-opacity-50 dark:bg-opacity-80 backdrop-blur-lg backdrop-saturate-150 text-darkText dark:text-text">
+          <div className="p-0 ml-8 text-center inline-flex items-center justify-center">
+            <span className="text-2xl font-bold">Reactants</span>
+          </div>
+          <div className="inline-flex font-normal text-xl font-roboto tracking-wider">
+            <li className="list-none mr-8 text-greyText hover:text-darkText dark:hover:text-text transition-all duration-200">
+              <Link href="/">Contribute</Link>
+            </li>
+            <li className="list-none text-greyText hover:text-darkText dark:hover:text-text transition-all duration-200">
+              <Link href="/">About</Link>
+            </li>
+          </div>
+          <div className="mr-8">
+            {session && (
+              <PrimaryOutlineButton handleOnClick={handleSignout} type="small">
+                Sign Out
+              </PrimaryOutlineButton>
+            )}
+            {!session && (
+              <PrimaryOutlineButton
+                handleOnClick={handleSignin}
+                type="small"
+                textColor="background"
+              >
+                Sign In
+              </PrimaryOutlineButton>
+            )}
+          </div>
+        </header>
       )}
-      {!session && (
-        <PrimaryButton
-          handleOnClick={handleSignin}
-          type="small"
-          textColor="background"
-        >
-          Sign In
-        </PrimaryButton>
+      {!navActive && (
+        <header className="fixed p-3 w-full h-fit flex items-center justify-between bg-background dark:bg-darkBackground  text-darkText dark:text-text">
+          <div className="p-0 ml-8 text-center inline-flex items-center justify-center">
+            <span className="text-2xl font-bold">Reactants</span>
+          </div>
+          <div className="inline-flex font-normal text-xl font-roboto tracking-wider">
+            <li className="list-none mr-8 text-greyText hover:text-darkText dark:hover:text-text transition-all duration-200">
+              <Link href="/">Contribute</Link>
+            </li>
+            <li className="list-none text-greyText hover:text-darkText dark:hover:text-text transition-all duration-200">
+              <Link href="/">About</Link>
+            </li>
+          </div>
+          <div className="mr-8">
+            {session && (
+              <PrimaryOutlineButton handleOnClick={handleSignout} type="small">
+                Sign Out
+              </PrimaryOutlineButton>
+            )}
+            {!session && (
+              <PrimaryOutlineButton
+                handleOnClick={handleSignin}
+                type="small"
+                textColor="background"
+              >
+                Sign In
+              </PrimaryOutlineButton>
+            )}
+          </div>
+        </header>
       )}
-    </header>
+    </>
   );
 };
 
